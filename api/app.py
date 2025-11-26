78203903c5c8c460753e952f8e0c9024111e86fc
Harrison Akunne <harrisonderick.65@gmail.com>
2025-11-26 16:42:56 +0100

    API ready

---
+++ api/app.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import os
+
+app = FastAPI()
+
+model = joblib.load(os.path.join("model", "classifier.pkl"))
+vectorizer = joblib.load(os.path.join("model", "vectorizer.pkl"))
+
+
+class NewsInput(BaseModel):
+    text: str
+
+
+@app.get("/")
+def home():
+    return {"message": "Fake News Detection API is running"}
+
+
+@app.post("/predict")
+def predict(data: NewsInput):
+    text_vec = vectorizer.transform([data.text])
+    prediction = model.predict(text_vec)[0]
+    prob = model.predict_proba(text_vec)[0]
+    return {
+        "prediction": "Real" if prediction == 1 else "Fake",
+        "fake_probability": round(prob[0], 3),
+        "real_probability": round(prob[1], 3),
+    }
